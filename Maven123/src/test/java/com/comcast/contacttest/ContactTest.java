308a14e087a59872d7ff50c514806ea3d64c5271
SHEPHALI <SHEPHALI@DESKTOP-T1QKRRO>
2021-03-15 12:58:55 +0530

    commit 1

---
+++ Maven123/src/test/java/com/comcast/contacttest/ContactTest.java
@@ -0,0 +1,24 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("execute create contact");	
+	}
+	
+	@Test
+	public void modifycontacttest()
+	{
+	 System.out.println("execute modify create contact");	
+	}
+	
+	@Test
+	public void deletecreatecontacttest()
+	{
+	 System.out.println("deletecreate contact");	
+	}
+	
+}
